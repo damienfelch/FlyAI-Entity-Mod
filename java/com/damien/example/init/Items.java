94af62e97d8d2ffcc402785be9e0f42db169cb20
Damien Felch <dfelch@usc.edu>
2025-05-03 20:30:42 -0700

    completed neurmomechfly entity

--- java/com/damien/example/init/Items.java
+++
@@ -1,8 +0,0 @@
-package com.damien.example.init;
-import com.damien.example.items.ItemBase;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.item.Item;
-
-public class Items {
-    public static final Item exampleItem = new ItemBase("example_item", CreativeTabs.MISC);
-}
--- java/com/damien/example/init/Mobs.java
+++ java/com/damien/example/init/Mobs.java
@@ -1,11 +1,9 @@
 package com.damien.example.init;
 
-import com.damien.example.entity.monster.EntityFly;
-import com.damien.example.entity.monster.EntitySpiderZombie;
+import com.damien.example.entity.monster.EntityCybug;
 import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.world.World;
 
 public class Mobs {
-    public static final EntityMob fly = new EntityFly(null);
-    public static final EntityMob spiderZombie = new EntitySpiderZombie(null);
+    public static final EntityMob cybug = new EntityCybug(null);
 }
